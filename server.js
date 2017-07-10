e280d24b4c79b672f5b2a6331fc942af3624449d
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-10 15:37:08 -0500

    Shell routes added

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 const port = process.env.port || 8080;
 const path = require("path");
 
+// MONGOOSE
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 const DB = "stattrackerDB";
 const dbUrl = `mongodb://localhost:27017/${DB}`;
 
+// CONNECT TO DB
 mongoose
   .connect(dbUrl)
   .then(() => {
@@ -16,6 +18,23 @@ mongoose
   .catch(err => {
     console.log(`Unable to connect: ${err}`);
   });
+
+// MIDDLEWARE
+
+// ROUTES
+app.get("/api/activities", (req, res) => {});
+
+app.post("/api/activities", (req, res) => {});
+
+app.get("/api/activities/:id", (req, res) => {});
+
+app.put("/api/activities/:id", (req, res) => {});
+
+app.delete("/api/activities/:id", (req, res) => {});
+
+app.post("/api/activities/:id/stats", (req, res) => {});
+
+app.delete("/api/stats/:id", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Spinning with express: Port ${port}`);
